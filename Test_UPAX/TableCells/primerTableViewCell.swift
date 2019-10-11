f4ff3db1bdaa9f5662198a08cfe3fd1d34c8219b
Eduardo Navarrete Carmona <nava.0892@gmail.com>
2019-10-11 01:10:54 -0500

    Se finaliza primer table view con su logica

---
+++ Test_UPAX/TableCells/primerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  primerTableViewCell.swift
+//  Test_UPAX
+//
+//  Created by Eduardo Navarrete Carmona on 10/10/19.
+//  Copyright © 2019 Eduardo Navarrete Carmona. All rights reserved.
+//
+
+import UIKit
+
+class primerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
