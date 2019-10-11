4353e2da405b97f27a166e3cc158eb69b41460b4
Eduardo Navarrete Carmona <nava.0892@gmail.com>
2019-10-11 01:10:54 -0500

    Se finaliza primer table view con su logica

---
+++ Test_UPAX/View_Controllers/PrimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrimerViewController.swift
+//  Test_UPAX
+//
+//  Created by Eduardo Navarrete Carmona on 10/10/19.
+//  Copyright © 2019 Eduardo Navarrete Carmona. All rights reserved.
+//
+
+import UIKit
+
+class PrimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
