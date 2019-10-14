fa9b05be334383534079d2fc53256c0fb9c92e3a
Eduardo Navarrete Carmona <nava.0892@gmail.com>
2019-10-13 21:14:31 -0500

    Se da por terminado el test por falta de tiempo

---
+++ Test_UPAX/TableCells/CamaraCell.swift
@@ -0,0 +1,26 @@
+//
+//  CamaraCell.swift
+//  Test_UPAX
+//
+//  Created by Eduardo Navarrete Carmona on 13/10/19.
+//  Copyright © 2019 Eduardo Navarrete Carmona. All rights reserved.
+//
+
+import UIKit
+
+class CamaraCell: UITableViewCell {
+
+    @IBOutlet weak var imgCamara: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Test_UPAX/TableCells/TelefonoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TelefonoCell.swift
+//  Test_UPAX
+//
+//  Created by Eduardo Navarrete Carmona on 13/10/19.
+//  Copyright © 2019 Eduardo Navarrete Carmona. All rights reserved.
+//
+
+import UIKit
+
+class TelefonoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
